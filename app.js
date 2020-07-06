5298fe1a07fe4dd22621f5b5e2afe87e44cca144
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-06 15:19:09 -0700

    working on event listener

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 const theeButton = document.getElementById('theOneButton');
 console.log(theeButton);
 
+theeButton.addEventListener ('click', () => {
+    const name = prompt('What is your name?');
+    const really = prompt('Wanna take the quiz?'); 
+        if (really === false) return;
+    const answer1 = prompt('Is the Flying V a metal guitar?');
+    const answer2 = prompt('Does OZZY rule?');
+    const answer3 = prompt('Is IPA metal?');
+
+    let score = 0;
+
+    if (countsAsAYes(answer1)) {
+        score++;
+    }
+
+    if (countsAsAYes(answer2)) {
+        score++;
+    }
+
+    if (!countsAsAYes(answer3)) {
+        score++;
+    }
+
+
+});
+
 // initialize state
 
 // set event listeners to update state and DOM
