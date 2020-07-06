51882b0e5977d9ad2194c73b11324c9b157f02e3
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-06 13:47:33 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// import functions and grab DOM elements
+const theeButton = document.getElementById('theOneButton');
+console.log(theeButton);
+
+// initialize state
+
+// set event listeners to update state and DOM
